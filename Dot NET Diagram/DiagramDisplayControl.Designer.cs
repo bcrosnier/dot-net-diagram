56a0010e8badf5635222d8097108baa4aabcbf6a
Benjamin Crosnier <bcrosnier@intechinfo.fr>
2012-10-22 15:11:26 +0200

    Test shape display

--- Dot NET Diagram/DiagramDisplayControl.Designer.cs
+++ Dot NET Diagram/DiagramDisplayControl.Designer.cs
@@ -29,13 +29,20 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiagramDisplayControl));
+            Dataweb.NShape.RoleBasedSecurityManager roleBasedSecurityManager4 = new Dataweb.NShape.RoleBasedSecurityManager();
             this._NShapeDisplay = new Dataweb.NShape.WinFormsUI.Display();
+            this._DiagramSetController = new Dataweb.NShape.Controllers.DiagramSetController();
+            this._NShapeProject = new Dataweb.NShape.Project(this.components);
+            this._CachedRepository = new Dataweb.NShape.Advanced.CachedRepository();
+            this._xmlStore = new Dataweb.NShape.XmlStore();
             this.SuspendLayout();
             // 
             // _NShapeDisplay
             // 
+            this._NShapeDisplay.AllowDrop = true;
             this._NShapeDisplay.BackColorGradient = System.Drawing.SystemColors.Control;
-            this._NShapeDisplay.DiagramSetController = null;
+            this._NShapeDisplay.DiagramSetController = this._DiagramSetController;
             this._NShapeDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
             this._NShapeDisplay.GridColor = System.Drawing.Color.Gainsboro;
             this._NShapeDisplay.GridSize = 19;
@@ -47,17 +54,46 @@
             this._NShapeDisplay.SelectionInactiveColor = System.Drawing.Color.Gray;
             this._NShapeDisplay.SelectionInteriorColor = System.Drawing.Color.WhiteSmoke;
             this._NShapeDisplay.SelectionNormalColor = System.Drawing.Color.DarkGreen;
-            this._NShapeDisplay.Size = new System.Drawing.Size(150, 150);
+            this._NShapeDisplay.Size = new System.Drawing.Size(500, 500);
             this._NShapeDisplay.TabIndex = 0;
             this._NShapeDisplay.ToolPreviewBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(119)))), ((int)(((byte)(136)))), ((int)(((byte)(153)))));
             this._NShapeDisplay.ToolPreviewColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(70)))), ((int)(((byte)(130)))), ((int)(((byte)(180)))));
+            // 
+            // _DiagramSetController
+            // 
+            this._DiagramSetController.ActiveTool = null;
+            this._DiagramSetController.Project = this._NShapeProject;
+            // 
+            // _NShapeProject
+            // 
+            this._NShapeProject.LibrarySearchPaths = ((System.Collections.Generic.IList<string>)(resources.GetObject("_NShapeProject.LibrarySearchPaths")));
+            this._NShapeProject.Name = null;
+            this._NShapeProject.Repository = this._CachedRepository;
+            roleBasedSecurityManager4.CurrentRole = Dataweb.NShape.StandardRole.Administrator;
+            roleBasedSecurityManager4.CurrentRoleName = "Administrator";
+            this._NShapeProject.SecurityManager = roleBasedSecurityManager4;
+            // 
+            // _CachedRepository
+            // 
+            this._CachedRepository.ProjectName = null;
+            this._CachedRepository.Store = this._xmlStore;
+            this._CachedRepository.Version = 0;
+            // 
+            // _xmlStore
+            // 
+            this._xmlStore.DesignFileName = "";
+            this._xmlStore.DirectoryName = ".";
+            this._xmlStore.FileExtension = ".xml";
+            this._xmlStore.ProjectName = "";
             // 
             // DiagramDisplayControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.Controls.Add(this._NShapeDisplay);
             this.Name = "DiagramDisplayControl";
+            this.Size = new System.Drawing.Size(500, 500);
             this.ResumeLayout(false);
 
         }
@@ -65,5 +101,9 @@
         #endregion
 
         private Dataweb.NShape.WinFormsUI.Display _NShapeDisplay;
+        private Dataweb.NShape.Project _NShapeProject;
+        private Dataweb.NShape.Controllers.DiagramSetController _DiagramSetController;
+        private Dataweb.NShape.Advanced.CachedRepository _CachedRepository;
+        private Dataweb.NShape.XmlStore _xmlStore;
     }
 }
--- Dot NET Diagram/DiagramDisplayControl.cs
+++ Dot NET Diagram/DiagramDisplayControl.cs
@@ -6,6 +6,10 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Dataweb.NShape;
+using Dataweb.NShape.Advanced;
+using Dataweb.NShape.GeneralShapes;
+using System.IO;
 
 namespace Dot_NET_Diagram
 {
@@ -14,9 +18,41 @@ namespace Dot_NET_Diagram
     /// </summary>
     public partial class DiagramDisplayControl : UserControl
     {
+        private Dataweb.NShape.Diagram _NShapeDiagram;
+
         public DiagramDisplayControl()
         {
             InitializeComponent();
+
+            _xmlStore.DirectoryName = System.IO.Path.GetDirectoryName( Application.ExecutablePath );
+            _xmlStore.FileExtension = ".nspj";
+            
+            _NShapeProject.Name = ".NET Diagram";
+            _NShapeProject.LibrarySearchPaths.Add( Path.Combine( System.IO.Path.GetDirectoryName( Application.ExecutablePath ), @"lib" ) );
+            _NShapeProject.AutoLoadLibraries = true;
+            _NShapeProject.AddLibraryByName( "Dataweb.NShape.GeneralShapes", true );
+
+
+            _NShapeDiagram = new Diagram( "diagram" );
+            _NShapeDiagram.Height = _NShapeDisplay.Height;
+            _NShapeDiagram.Width = _NShapeDisplay.Width;
+
+            _NShapeProject.Create();
+            
+            /* Test display */
+            RectangleBase shape = (RectangleBase)_NShapeProject.ShapeTypes["Ellipse"].CreateInstance();
+
+            shape.Width = 100;
+            shape.Height = 100;
+
+            shape.X = 50;
+            shape.Y = 50;
+            shape.SetCaptionText( 0, "Hello world" );
+
+            _NShapeDiagram.Shapes.Add( shape );
+            /* End of display */
+
+            _NShapeDisplay.Diagram = _NShapeDiagram;
         }
     }
 }
